3bafd0b95411549f2ac99fd8973bffe0ce5b836a
DavidZhang91 <davidzhang@91app.com>
2018-10-31 06:38:21 +0800

    upgrade deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #deploy.sh
 #!/bin/bash
+
+#### Step1 : update public Folder ####
 echo -e "\033[0;32mDeploying Public updates to GitHub...\033[0m"
 
 # Build the project.
@@ -23,6 +25,8 @@ git push origin master
 
 echo -e "\033[0;32mDeploying Public updates to GitHub Done...\033[0m"
 
+#### Step2 : update quickBlog Folder ####
+
 # Come Back
 cd ..
 
@@ -42,3 +46,31 @@ git commit -m "$msg"
 git push origin master
 
 echo -e "\033[0;32mDeploying quickBlog updates to GitHub Done...\033[0m"
+
+#### Step3 : update content/post Folder ####
+
+# Come content/post
+cd content/post
+
+echo -e "\033[0;32mDeploying content/post updates to GitHub...\033[0m"
+
+# Add changes to git.
+git add -A
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
+echo -e "\033[0;32mDeploying content/post updates to GitHub Done...\033[0m"
+
+# Come Back post to content
+cd ..
+
+# Come Back content to quickBlog
+cd ..
